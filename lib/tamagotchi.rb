65d4ed5f1ab878d306226b2d61811b71ca6be288
James Maynard <jameskirkmaynard@gmail.com>
2015-01-13 16:23:06 -0800

    Add specs and methods to make a Tamagotchi class work in Ruby. Add feeding, activity, and sleeping methods.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -6,26 +6,80 @@ class Tamagotchi
     @sleep_level = 10
     @activity_level = 10
 
+
   end
+
+  define_method(:time_passes) do
+    @food_level = @food_level-2
+    @sleep_level = @sleep_level-2
+    @activity_level = @activity_level-3
+  end
+
+#name properties
 
   define_method(:name) do
     @name
   end
 
+#food methods
+
   define_method(:food_level) do
     @food_level
   end
+
+  define_method(:feeding_time) do
+    @food_level = 10
+    "Yum-yum"
+  end
+
+  define_method(:is_alive?) do
+    if @food_level>=1
+      "still kicking"
+    else
+      "totally dead"
+    end
+  end
+
+  define_method(:is_dead?) do
+    if @food_level>=1
+      "not yet"
+    else
+      "as a doornail"
+    end
+  end
+
+#sleep methods
 
   define_method(:sleep_level) do
     @sleep_level
   end
 
+  define_method(:bed_time) do
+    @sleep_level = 10
+  end
+
+  define_method(:no_sleep?) do
+    if @sleep_level == 10
+      "good to go!"
+    elsif @sleep_level<=5
+      "yaaawwwn"
+    else @sleep_level<=1
+      "sleep depravity led to madness and your Tamagotchi has eaten you."
+    end
+  end
+
+#activity methods
+
   define_method(:activity_level) do
     @activity_level
   end
 
-  define_method(:time_passes) do
-    
+  define_method(:take_a_walk) do
+    @activity_level = 10
   end
+
+
+
+
 
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -17,6 +17,28 @@ describe("Tamagotchi")
   describe("#time_passes") do
     it("decreases the amount of food the Tamagotchi has left by 1") do
       my_pet = Tamagotchi.new("lil Otto III")
+      my_pet.time_passes()
       expect(my_pet.food_level()).to(eq(9))
+    end
+  end
+
+  describe("#is_alive?") do
+    it("is alive if the food level is above 0") do
+      my_pet = Tamagotchi.new("lil Otto III")
+      expect(my_pet.is_alive?()).to(eq("still kicking"))
+    end
+  end
+
+  describe("#is_dead?") do
+    it("is dead if the food level is at 0") do
+      my_pet = Tamagotchi.new("lil Otto III")
+      expect(my_pet.is_dead?()).to(eq("not yet"))
+    end
+  end
+
+  describe("#feeding_time") do
+    it("resets the food level back to 10") do
+      my_pet = Tamagotchi.new("lil Otto III")
+      expect(my_pet.food_level()).to(eq(10))
     end
   end
